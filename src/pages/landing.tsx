c68eea5a98c1f2547ee9c94905c81ae58da8cead
sprite-enjoyer <samkharadze.saba01@gmail.com>
2023-02-04 17:03:10 +0400

    #5 name, location stored locally

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,8 +1,32 @@
 import Header from "@/components/Header";
 import styles from "../styles/landingPage.module.scss";
-import { Button, Card, Container, Grid, Input, Link, Spacer, Text } from "@nextui-org/react";
+import { Button, Container, Input, PressEvent, Spacer } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 
 const LandingPage = () => {
+
+  const [username, setUsername] = useState('');
+  const [location, setLocation] = useState('');
+
+  const onClickHandler = (e: PressEvent) => {
+    console.log(username);
+    console.log(location);
+
+    localStorage.setItem("KIUexchange_username", username);
+    localStorage.setItem("KIUexchange_location", location);
+
+    console.log(localStorage.getItem("KIUexchange_username"));
+    console.log(localStorage.getItem("KIUexchange_location"));
+
+  };
+
+  useEffect(() => {
+
+    setUsername(localStorage.getItem("KIUexchange_username") ?? "");
+    setLocation(localStorage.getItem("KIUexchange_location") ?? "");
+
+  }, []);
+
   return (
     <>
       <Header />
@@ -16,22 +40,26 @@ const LandingPage = () => {
           alignItems="center"
         >
           <Input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             css={{ width: "60vw", "@xs": { width: "15vw" } }}
             underlined
             labelPlaceholder="Username"
+            as="input"
           />
           <Spacer y={2} />
           <Input
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
             css={{ width: "60vw", "@xs": { width: "15vw" } }}
             underlined
             labelPlaceholder="location (optional)"
+            as="input"
           />
           <Spacer y={1.5} />
-          <Button css={{ backgroundColor: "$lightGreen", outline: "transparent" }} auto>continue</Button >
+          <Button onPress={onClickHandler} css={{ backgroundColor: "$lightGreen", outline: "transparent" }} auto>continue</Button >
         </Container>
       </div>
-
-
     </>
   );
 }
